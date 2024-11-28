0264a1173cc87b2c1ac6e62718bdfc1444684c5a
chris1797 <jaehun6004@gmail.com>
2024-11-28 23:54:09 +0900

    feat: primitive, literal type

---
+++ src/primitive&literal.ts
@@ -0,0 +1,38 @@
+// Number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 3.14;
+let num4: number = -2.14e10;
+let num5: number = 0x123;
+let num6: number = Infinity; // 무한대
+let num7: number = -Infinity; //
+let num8: number = NaN; // Not a Number
+
+num1 = 'string'; // Errors
+num1.toUpperCase(); // Errors
+
+
+// String
+let str1: string = 'Hello';
+let str2: string = "World";
+let str3: string = `Hello, ${num1}`;
+
+str.toFixed(); // Errors
+
+
+// Boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+bool1 = 1; // Errors
+
+// Undefined & Null
+let null1: null = null;
+let undefined1: undefined = undefined;
+
+/* ===================== Literal Types ===================== */
+let status: 'success' | 'fail';
+
+let numA: 10 = 10;
+numA = 20; // Errors
+
