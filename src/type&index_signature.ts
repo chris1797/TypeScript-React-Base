4d4736cde5134dbd9b8ed50d3d240297234b5071
chris1797 <jaehun6004@gmail.com>
2024-12-01 00:04:40 +0900

    feat: type, index signature

---
+++ src/type&index_signature.ts
@@ -0,0 +1,23 @@
+// Type Signature
+
+type User= {
+    id: number;
+    name: string;
+    nickname?: string;
+    birth: string;
+    location: string;
+}
+
+let user: User = {
+    id: 1,
+    name: 'Chris',
+    birth: '1992-01-01',
+    location: 'Seoul'
+}
+
+let user2: User = {
+    id: 2,
+    name: 'Tom',
+    birth: '1993-01-01',
+    location: 'Busan'
+}
