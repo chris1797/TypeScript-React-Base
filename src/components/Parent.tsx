317277613e033a72f07895d35de36d790cea4a74
chris1797 <jaehun6004@gmail.com>
2025-09-14 16:37:59 +0900

    feat: add flex

--- src/components/Parent.tsx
+++ src/components/Parent.tsx
@@ -16,7 +16,35 @@ const Parent: React.FC = () => {
       <button onClick={() => setShowA(!showA)}>
         {showA ? 'B 컴포넌트 보기' : 'A 컴포넌트 보기'}
       </button>
-      {showA ? <AComponent message="Hello from AComponent!" /> : <BComponent />}
+      <p>
+        {showA ? (
+          <AComponent message="Hello from AComponent!" />
+        ) : (
+          <BComponent />
+        )}
+      </p>
+      <p
+        style={{
+          border: '1px solid black',
+        }}
+      ></p>
+
+      <h2> Flex 연습 </h2>
+      <div
+        style={{
+          backgroundColor: 'yellow',
+          display: 'flex',
+          justifyContent: 'space-around',
+          alignItems: 'end',
+        }}
+      >
+        <div style={{ backgroundColor: 'lightblue' }}>
+          <h2>스타일이 적용된 div</h2>
+        </div>
+        <div style={{ backgroundColor: 'lightgreen' }}>
+          <h2>스타일이 적용된 div2</h2>
+        </div>
+      </div>
     </div>
   );
 };
